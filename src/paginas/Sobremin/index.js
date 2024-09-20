be53e262829570578d70c56bf8b1369fb8c2b313
RafaelSantos <87025218+ASLRafa@users.noreply.github.com>
2024-09-20 17:09:30 +0000

    feat:Animating route transition using framer-motion

--- src/paginas/Sobremin/index.js
+++ src/paginas/Sobremin/index.js
@@ -1,7 +1,11 @@
+import AnimatedPage from "componentes/Animated/animatedPage";
+
 export default function SobreMin() {
   return (
-    <div>
-      <h1>Sobre Min Page</h1>
-    </div>
+    <AnimatedPage>
+      <div>
+        <h1>Sobre Min Page</h1>
+      </div>
+    </AnimatedPage>
   );
 }
--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,10 +1,13 @@
+import AnimatedPage from "componentes/Animated/animatedPage";
 import Banner from "componentes/Banner";
 
 export default function SobreMin() {
   return (
-    <main>
-      <Banner></Banner>
-      <h1>Olá Mundo Page</h1>
-    </main>
+    <AnimatedPage>
+      <main>
+        <Banner></Banner>
+        <h1>Olá Mundo Page</h1>
+      </main>
+    </AnimatedPage>
   );
 }
