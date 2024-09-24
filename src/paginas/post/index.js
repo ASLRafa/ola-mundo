13edea8e16c3b6c54b4077e2c4f26a7917b07c72
RafaelSantos <87025218+ASLRafa@users.noreply.github.com>
2024-09-24 13:52:53 +0000

    PostCard no final de cada post

--- src/paginas/post/index.js
+++ src/paginas/post/index.js
@@ -6,9 +6,14 @@ import "./post.css";
 import { AnimatedPagePost } from "componentes/Animated/animatedPage";
 import NaoEncontrada from "paginas/naoEncontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import PostCard from "componentes/PostCard";
 
 export default function Post() {
   const parametros = useParams();
+  const listaFiltrada = posts.filter(
+    (post) => post.id !== Number(parametros.id)
+  );
+
   const post = posts.find((post) => {
     return post.id === Number(parametros.id);
   });
@@ -22,14 +27,26 @@ export default function Post() {
           <Route
             index
             element={
-              <PostModelo
-                fotoCapa={`/assets/posts/${post.id}/capa.png`}
-                titulo={post.titulo}
-              >
-                <div className="post-markdown-container">
-                  <ReactMarkdown>{post.texto}</ReactMarkdown>
-                </div>
-              </PostModelo>
+              <div>
+                <PostModelo
+                  fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                  titulo={post.titulo}
+                >
+                  <div className="post-markdown-container">
+                    <ReactMarkdown>{post.texto}</ReactMarkdown>
+                    <h2 className="subtitulo"> Veja esses outros Posts </h2>
+                    <ul className="posts">
+                      {listaFiltrada.slice(0, 4).map((post) => {
+                        return (
+                          <li key={post.id}>
+                            <PostCard post={post} />
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </PostModelo>
+              </div>
             }
           ></Route>
         </Route>
